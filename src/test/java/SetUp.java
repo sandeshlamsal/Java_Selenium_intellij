ec564d6ffc420eb03f77a2fe3dd07d220cc24d06
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2018-09-27 22:10:36 -0600

    first commit

---
+++ src/test/java/SetUp.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SetUp {
+
+    @Test
+    public void OpenBrowser() {
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+        WebDriver wd=new ChromeDriver();
+        wd.get("http://www.gmail.com");
+    }
+}
